34a0c767c40bbbdd1b718a857ed997feb10d47a0
David Long <techrud.bnd@gmail.com>
2026-09-10 23:25:40 -0400

    Delete generated-client directory
    unneeded

--- generated-client/openapi_client/api/__init__.py
+++
@@ -1,7 +0,0 @@
-# flake8: noqa
-
-# import apis into api package
-from openapi_client.api.pet_api import PetApi
-from openapi_client.api.store_api import StoreApi
-from openapi_client.api.user_api import UserApi
-
